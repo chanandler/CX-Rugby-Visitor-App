ea2e55d4490b8fc6b20ccfc2337f044006b3444e
Clint Yarwood <chanandler@me.com>
2026-04-19 12:25:25 +0100

    Initial Commit

---
+++ CX Rugby Visitor App/CX_Rugby_Visitor_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CX_Rugby_Visitor_AppApp.swift
+//  CX Rugby Visitor App
+//
+//  Created by Clint Yarwood on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CX_Rugby_Visitor_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CX Rugby Visitor App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CX Rugby Visitor App
+//
+//  Created by Clint Yarwood on 19/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
